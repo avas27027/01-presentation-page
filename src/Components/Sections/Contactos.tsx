a666406be74eab170f7d0037937bfc27ed4f921c
avas27 <alvas27027@users.noreply.github.com>
2023-02-01 16:23:06 -0500

    second commit

--- src/Components/Sections/Contactos.tsx
+++ src/Components/Sections/Contactos.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Contactos(props:{view: (node: Element | null) => void}) {
   return (
-    <section className='contactos' ref={props.view}>Contactos</section>
+    <section className='contactos' ref={props.view}>
+        <div className='contactos box'>
+            <div className='contactos-card'></div>
+            <div className='contactos-card'></div>
+            <div className='contactos-card'></div>
+            <div className='contactos-card'></div>
+        </div>
+    </section>
   )
 }
